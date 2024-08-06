a98f0842bd9a2cca0e9e44fe47e7622805c1b1e4
quangvh18 <quangvh18@ghtk.co>
2024-08-06 14:47:47 +0700

    update

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -6,7 +6,7 @@ import NavLinks from "../NavLinks";
 export default function Header() {
   return (
     <header>
-      <div className="container mx-auto max-w-[1038px] flex justify-between items-center m-4">
+      <div className="container mx-auto px-4 max-w-[1038px] flex justify-between items-center m-4">
         <div className="flex flex-col">
           <img src="/images/logo.png" alt="Logo Wifi Socks" className="h-12" />
         </div>
